68dd502d63fa2188ca57f979b7b603ca5fa2fc06
Willy-Tseng-200365 <56176606+Willy-Tseng-200365@users.noreply.github.com>
2020-01-27 15:06:37 -0800

    heck

--- myframeDemo.java
+++ myframeDemo.java
@@ -36,8 +36,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
  
-/* FrameDemo.java requires no other files. */
-public class FrameDemo {
+/* myframeDemo.java requires no other files. */
+public class myframeDemo {
     /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
@@ -45,16 +45,16 @@ public class FrameDemo {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("FrameDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame myframe = new JFrame("Hello World");
+        myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
         JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(175, 100));
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        emptyLabel.setPreferredSize(new Dimension(3840, 2160));
+        myframe.getContentPane().add(emptyLabel, BorderLayout.CENTER);
  
         //Display the window.
-        frame.pack();
-        frame.setVisible(true);
+        myframe.pack();
+        myframe.setVisible(true);
     }
  
     public static void main(String[] args) {
